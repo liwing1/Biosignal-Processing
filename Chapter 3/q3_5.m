311bb8ed309ed42a0fc4100b29a8b0edd36c36b0
Li Wing <liwramos1@gmail.com>
2021-05-05 19:48:45 -0300

    chap4

--- Chapter 3/q3_5.m
+++ Chapter 3/q3_5.m
@@ -6,18 +6,15 @@ t = (0:N-1)/fs;
 y = 0.5*sawtooth(2*2*pi*t);
 yf = fft(y);
 
-a_0 = mean(y);
-for j = 1:26
-    y_mag(j) = 2*abs(yf(j+1)/N);
-    y_ph(j) = angle(yf(j+1));
+Y = zeros(1, N);
+for j = 1:260
+    y_mag(j) = 2*abs(yf(j)/N);
+    y_ph(j) = angle(yf(j));
+    
+    f(j) = 1*(j-1);
+    Y = Y + y_mag(j)*cos(2*pi*f(j)*t + y_ph(j));
 end
 
-Y = zeros(1, N);
-for j = 1:26
-    f(j) = 2*j;
-    Y = Y + y_mag(j)*cos(2*pi*j*t + y_ph(j));
-end
-Y = a_0 + Y;
 
 plot(t,y);hold on;
 plot(t,Y);
